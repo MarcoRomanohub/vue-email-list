8ae04d11ecadb8aa967f12b50b11dbc047d4b449
”MarcoRomanoHub” <marcoromano270@gmail.com>
2024-03-19 20:25:35 +0100

    [add] emails console log

--- script.js
+++ script.js
@@ -4,13 +4,30 @@ const { createApp } = Vue;
 createApp({
   data(){
     return{
-      
+      apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail',
+      risultato: '',
+      items: 10
     }
   },
   methods:{
 
+    getApi(){
+
+      axios.get(this.apiUrl + `?items=${this.items}`)
+      .then((res)=> {
+        console.log(res.data);
+        this.risultato = res.data.response
+      })
+      .catch(error => {
+        console.log(error.message);
+      })
+
+    }
+
   },
   mounted(){
+
+    // this.getApi();
 
   }
 }).mount('#app')
